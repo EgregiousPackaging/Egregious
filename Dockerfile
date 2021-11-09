db323a830305b3739afcfe72cdf75506a865bfe8
Jon Linnell <jonlinnell@users.noreply.github.com>
2021-11-09 21:51:57 +0000

    Add Dockerfile and basic docker-compose

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:14.18-bullseye AS basebuild
+WORKDIR /app
+
+COPY . .
+
+RUN npm ci --include=dev && npm run build
+
+FROM node:14.18-alpine AS prod
+WORKDIR /app
+
+ENV NODE_END=production
+
+COPY --from=basebuild /app/dist/ ./dist/
+
+COPY package*.json ./
+
+RUN apk add --no-cache curl && \
+  npm ci --only=prod --no-optional
+
+USER node
+
+HEALTHCHECK --interval=3m --timeout=5s --retries=3 CMD [ "curl", "--fail", "http://localhost:5000/healthcheck" ]
+
+CMD ["node", "dist/server/start.js"]
--- src/server/start.ts
+++ src/server/start.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express"
 
+import routeHealthcheck from "./routes/healthcheck"
+
 const badDb = new Map()
 
 const app = express()
@@ -19,7 +21,20 @@ app.post("/api/report", (req: Request, res: Response) => {
   res.json(Object.fromEntries(badDb)).status(200)
 })
 
+// @TODO: There's funky things we can do to gracefully drain connections, but that's way out of
+// scope for now.
+process.on("SIGINT", () => {
+  console.log("Received SIGINT, terminating semi-gracefully.")
+  process.exit(0)
+})
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, terminating semi-gracefully.")
+  process.exit(0)
+})
+
 app.use("/", express.static("dist/public"))
+app.use("/healthcheck", routeHealthcheck)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log("App is listening on port " + port))
