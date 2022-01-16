b22c6cf875d64a2119f06635ff40194ccfb34d6b
Mehow <kaiser.mehow@gmail.com>
2022-01-16 15:33:49 +0000

    fix backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,8 +9,11 @@ backupFile="${day}_backup.tar.gz"
 ssh debian@51.195.149.81 "docker run --rm -v egregiousdb:/egregiousdb -v /backups:/backups ubuntu tar czf /backups/$backupFile /egregiousdb"
 scp debian@51.195.149.81:/backups/$backupFile ./backups/$backupFile
 # test it worked
-tar -xzf ./backups/$backupFile
-if [ ! -f ./backups/$backupFile/backup/sqlite.db ]; then
+mkdir test-backup
+tar -xzf ./backups/$backupFile -C ./test-backup
+if [ ! -f ./test-backup/egregiousdb/db.sqlite ]; then
     echo "Backup failed, sqlite.db not found!"
     exit 1
 fi
+rm -r ./test-backup
+echo "Backup success!"
